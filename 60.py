f4daddbe431ce47fcb4a7f0650523e804892bda6
cw <981618298@qq.com>
2020-11-26 16:39:26 +0800

    second commit

--- 60.py
+++ 60.py
@@ -4,10 +4,10 @@ class Solution:
         dp = {}    # dp为当前点数之和：概率的字典
         dp[0] = 1
         for i in range(1, n+1):
-            newdp = collections.defaultdict(int)
-            for sm in dp:
-                for v in range(1,7):
-                    newdp[sm+v] += dp[sm] / 6
+            newdp = collections.defaultdict(int)    # 每次重新赋值
+            for sm in dp:              # 前n-1个骰子的6个点数（1~6）
+                for v in range(1,7):    # 新加的骰子的6个点数（1~6）
+                    newdp[sm+v] += dp[sm] / 6    # sm+v即为最终的sum， 累加和
             dp = newdp
         res = []
         for sm in range(n, 6*n+1):  # n个骰子，点数最小之和为n，最大为6*n
